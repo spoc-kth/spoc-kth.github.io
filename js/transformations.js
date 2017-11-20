a9f1b8721868e65e017cfc703c0f070ee435bbad
tomasalbrecht <tomasalb@kth.se>
2017-11-20 20:34:31 +0100

    change-bg

--- js/transformations.js
+++ js/transformations.js
@@ -2,7 +2,7 @@
 */
 var slides = document.querySelectorAll('#slides .slide');
 var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,4000000000000);
+var slideInterval = setInterval(nextSlide,10000);
 
 function nextSlide(){
 	slides[currentSlide].className = 'slide';
